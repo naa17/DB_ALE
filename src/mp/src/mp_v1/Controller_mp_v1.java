371789137f777108536a709b76b54a5aa7ad38fc
admin <admin@dyn3189-65.wlan.ic.ac.uk>
2019-12-02 15:53:32 +0000

    Renamed all variables + organise folders

---
+++ src/mp/src/mp_v1/Controller_mp_v1.java
@@ -0,0 +1,56 @@
+package mp_v1;
+
+import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+    public class Controller_mp_v1 {
+        @FXML
+        public TextField emailField;
+        @FXML
+        public TextField passwordField;
+        @FXML
+        public Button login;
+
+
+        @FXML
+        public void handle(ActionEvent actionEvent) throws IOException {
+            String email = emailField.getText();
+            String passw = passwordField.getText();
+            System.out.println(email);
+            System.out.println(passw);
+
+            List<String> loginDetails = new ArrayList<String>();
+            loginDetails.add(email);
+            loginDetails.add(passw);
+            System.out.println(loginDetails);
+
+            File file = new File("logindetails.csv");
+            if (file.createNewFile()){
+                System.out.println("Done");
+
+                FileWriter csvWriter = new FileWriter("logindetails.csv");
+                csvWriter.append(loginDetails.get(0));
+                csvWriter.append(",");
+                csvWriter.append(loginDetails.get(1));
+
+                csvWriter.flush();
+                csvWriter.close();
+
+                List<String> email_pass = CSVreader_mp.readCSV("logindetails.csv");
+
+            }else System.out.println("You have a problem :(");
+
+        }
+    }
+
+
+
+
--- src/sample/Controller.java
+++
@@ -1,9 +0,0 @@
-package sample;
-
-import javafx.fxml.FXML;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-
-public class Controller
-{
-}
